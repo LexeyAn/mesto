6842c5976dc0654bf6f8fc6e53d95f1330390ef9
Aleksey Anisimov <lexey.66@yandex.ru>
2022-12-06 22:47:56 +0300

    правки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,20 @@ let profile__button = document.querySelector('.profile__button');
 let popup = document.querySelector('.popup');
 let page = document.querySelector('.page');
 
-// функция добавляет селектор, делающий видимым/невидимым попап
-function openPopup() {
-  popup.classList.toggle('popup_opened');
-  page.classList.toggle('page_popup');
-};
-
-// слушатель кнопки редактирования профиля
-profile__button.addEventListener('click', openPopup);
-
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__nameInput');
-let jobInput = document.querySelector('.popup__jobInput');
+let nameInput = document.querySelector('.popup__text_box_nameInput');
+let jobInput = document.querySelector('.popup__text_box_jobInput');
 let profile__name = document.querySelector('.profile__name');
 let profile__profession = document.querySelector('.profile__profession');
+
+let popup__close = document.querySelector('.popup__close');
+
+// функция добавляет селектор, делающий видимым/невидимым попап
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profile__name.textContent;
+  jobInput.value = profile__profession.textContent;
+};
 
 // Обработчик «отправки» формы
 function handleFormSubmit(evt) {
@@ -27,21 +27,21 @@ function handleFormSubmit(evt) {
   profile__name.textContent = name;
   profile__profession.textContent = job;
 
-  openPopup();
+  closePopup();
 }
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
 
 // слушатель кнопки отправки формы
 formElement.addEventListener('submit', handleFormSubmit);
 
 // закрытие попапа
-let popup__close = document.querySelector('.popup__close');
 popup__close.addEventListener('click', closePopup);
 
-function closePopup() {
-  openPopup();
-  let name = profile__name.textContent;
-  let job =  profile__profession.textContent;
-  nameInput.value = name;
-  jobInput.value = job;
-}
+// слушатель кнопки редактирования профиля
+profile__button.addEventListener('click', openPopup);
+
 
